f7da620931cfb7d9efb18f92ef324f15ee292926
phoebe <jzou89260@gmail.com>
2022-12-18 16:51:08 -0500

    add file

---
+++ src/main/java/googlesheetdb/googlesheetdb/GoogleSheetDbApplication.java
@@ -0,0 +1,13 @@
+package googlesheetdb.googlesheetdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleSheetDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleSheetDbApplication.class, args);
+    }
+
+}
